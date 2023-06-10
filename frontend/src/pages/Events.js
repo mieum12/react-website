6d29fdf838f9f8a742158d1b6d7d0c9d787a118e
mieum12 <qpdlqltb1215@gmail.com>
2023-06-11 03:33:43 +0900

    6/11 페이지 로딩 시 defer 함수 사용

--- frontend/src/pages/Events.js
+++ frontend/src/pages/Events.js
@@ -1,26 +1,26 @@
-import { useLoaderData, json } from "react-router-dom";
+import { useLoaderData, json, defer, Await } from "react-router-dom";
 import EventsList from "../components/EventsList";
+import { Suspense } from "react";
 
+//데이터가 도착하기 전 이 컴포넌트를 로딩, 렌더링하고 데이터가 오기 전까지 폴백을 표시할 것
 function EventsPage() {
-  const data = useLoaderData();
+  const { events } = useLoaderData(); //여기에 아래에서 설정한 events key가 있음
 
-  //에러가 났을때 내보내는 것
-  // if (data.isError) {
-  //   <p>{data.message}</p>;
-  // }
-  const events = data.events;
-
-  return <EventsList events={events} />;
+  //Await은 데이터가 올때까지 기다리고, 데이터가 도착하면 즉, promise가 resolving되고 그 데이터가 도착하면 리액트 라우터가 실행 할 함수가 됨
+  return (
+    // Suspense는 다른 데이터가 도착하길 기다리는 동안 폴백을 보여주는 상황에서 사용
+    <Suspense fallback={<p style={{ textAlign: "center" }}>Loading...</p>}>
+      <Await resolve={events}>
+        {/* 이 함수는 데이터가 도착하면 리액트 라우터에 의해 호츌*/}
+        {(loadedEvents) => <EventsList events={loadedEvents} />}
+      </Await>
+    </Suspense>
+  );
 }
 
 export default EventsPage;
 
-//loader() 사용하기!!
-//우리가 그 페이지로 이동하기 시작할 때, 실제로 가기 전에 호출되는 함수이다, 페이지 컴포넌트가 랜더링 된 다음이 아님!!
-//백엔드 코드같지만 서버 코드가 아닌 클라이언트 코드
-//모든 브라우져 API도 접근 가능 (localStorage, cookies ...)
-//근데 react hook은 사용 불가(useState...), 리액트 컴포넌트 안에서만 사용 가능하기 때문, loader 함수는 리액트 컴포넌트가 아님
-export async function loader() {
+export async function loadEvents() {
   const response = await fetch("http://localhost:8080/events");
 
   //위에서 받은 response 응답을 loader에서 리턴하는 것이다
@@ -41,6 +41,20 @@ export async function loader() {
       { status: 500 }
     );
   } else {
-    return response;
+    const resData = await response.json();
+    return resData.events;
   }
+}
+
+//loader() 사용하기!!
+//우리가 그 페이지로 이동하기 시작할 때, 실제로 가기 전에 호출되는 함수이다, 페이지 컴포넌트가 랜더링 된 다음이 아님!!
+//백엔드 코드같지만 서버 코드가 아닌 클라이언트 코드
+//모든 브라우져 API도 접근 가능 (localStorage, cookies ...)
+//근데 react hook은 사용 불가(useState...), 리액트 컴포넌트 안에서만 사용 가능하기 때문, loader 함수는 리액트 컴포넌트가 아님
+export function loader() {
+  return defer({
+    // loadEvents는 promise를 리턴, 그것을 events key에 저장
+    // promise가 있어야 defer(=연기)가능
+    events: loadEvents(),
+  });
 }
