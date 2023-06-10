e301b25ea6b2a5d7177b8332e09eece0858d460f
mieum12 <qpdlqltb1215@gmail.com>
2023-06-11 01:27:24 +0900

    6/11 action()을 사용해 백엔드로 데이터 전송

--- frontend/src/pages/EventDetail.js
+++ frontend/src/pages/EventDetail.js
@@ -1,15 +1,29 @@
-import { useParams } from "react-router-dom";
+import { json, useRouteLoaderData } from "react-router-dom";
+import EventItem from "../components/EventItem";
 
 function EventDetailPage() {
   //useParams는 현재 활성 중인 라우트 파라미터에 접근 가능하다, URL에 인코딩된 값들에 접근해서 다이나믹 라우터 사용 가능
+  // const params = useParams();
 
-  const params = useParams();
-  return (
-    <>
-      <h1>Event Detail page!</h1>
-      <p>Event ID :{params.eventId}</p>
-    </>
-  );
+  //routeId를 인자로 받는 리액트 훅
+  const data = useRouteLoaderData("event-detail");
+
+  return <EventItem event={data.event} />;
 }
 
 export default EventDetailPage;
+
+export async function loader({ request, params }) {
+  const id = params.eventId;
+
+  const response = await fetch("http://localhost:8080/events/" + id);
+
+  if (!response.ok) {
+    throw json(
+      { message: "could not fetch details for selected event!" },
+      { status: 500 }
+    );
+  } else {
+    return response;
+  }
+}
--- frontend/src/pages/NewEvent.js
+++ frontend/src/pages/NewEvent.js
@@ -1,5 +1,37 @@
+import { json, redirect } from "react-router-dom";
+import EventForm from "../components/EventForm";
+
 function NewEventPage() {
-  return <h1>NewEvent page!</h1>;
+  return <EventForm />;
 }
 
 export default NewEventPage;
+
+//데이터를 백엔드로 제출하는 것을 해주는 action() 함수
+//제출된 폼 데이터를 추출한다
+export async function action({ request, params }) {
+  const data = await request.formData();
+
+  //get()으로 가져오는 것은 input form에서의 name이 된다
+  // const enteredTitle = data.get('title') 이렇게 각각 할수도 있지만
+  const eventData = {
+    title: data.get("title"),
+    image: data.get("image"),
+    date: data.get("date"),
+    description: data.get("description"),
+  };
+
+  const response = await fetch("http://localhost:8080/events", {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+    },
+    body: JSON.stringify(eventData),
+  });
+
+  if (!response.ok) {
+    throw json({ message: "Could not save event.." }, { status: 500 });
+  }
+
+  return redirect("/events");
+}
